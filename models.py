43f027f46fd85b6467418079238b2e9327a4ec98
niedu02 <duane.nielsen.rocks@gmail.com>
2018-10-04 22:23:27 -0700

    moved instruments to mentality built a new model

--- models.py
+++ models.py
@@ -4,63 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Encoder(nn.Module):
-    def __init__(self, input_shape, z_size, channels=3, first_kernel=5, first_stride=2, second_kernel=5, second_stride=2):
-        nn.Module.__init__(self)
-        # batchnorm in autoencoding is a thing
-        # https://arxiv.org/pdf/1602.02282.pdf
 
-        from mentalitystorm.util import conv_output_shape
-
-        # encoder
-        self.e_conv1 = nn.Conv2d(channels, 32, kernel_size=first_kernel, stride=first_stride)
-        self.e_bn1 = nn.BatchNorm2d(32)
-        output_shape = conv_output_shape(input_shape, kernel_size=first_kernel, stride=first_stride)
-
-        self.e_conv2 = nn.Conv2d(32, 128, kernel_size=second_kernel, stride=second_stride)
-        self.e_bn2 = nn.BatchNorm2d(128)
-        output_shape = conv_output_shape(output_shape, kernel_size=second_kernel, stride=second_stride)
-
-        self.e_conv3 = nn.Conv2d(128, 128, kernel_size=second_kernel, stride=second_stride)
-        self.e_bn3 = nn.BatchNorm2d(128)
-        self.z_shape = conv_output_shape(output_shape, kernel_size=second_kernel, stride=second_stride)
-
-        self.e_mean = nn.Conv2d(128, z_size, kernel_size=self.z_shape, stride=1)
-        self.e_logvar = nn.Conv2d(128, z_size, kernel_size=self.z_shape, stride=1)
-
-    def forward(self, x):
-        encoded = F.relu(self.e_bn1(self.e_conv1(x)))
-        encoded = F.relu(self.e_bn2(self.e_conv2(encoded)))
-        encoded = F.relu(self.e_bn3(self.e_conv3(encoded)))
-        mean = self.e_mean(encoded)
-        logvar = self.e_logvar(encoded)
-        return mean, logvar
-
-
-class Decoder(nn.Module):
-    def __init__(self, z_size, z_shape, channels=3, first_kernel=5, first_stride=2, second_kernel=5, second_stride=2):
-        nn.Module.__init__(self)
-
-        # decoder
-        self.d_conv1 = nn.ConvTranspose2d(z_size, 128, kernel_size=z_shape, stride=1)
-        self.d_bn1 = nn.BatchNorm2d(128)
-
-        self.d_conv2 = nn.ConvTranspose2d(128, 128, kernel_size=second_kernel, stride=second_stride,
-                                          output_padding=(1, 0))
-        self.d_bn2 = nn.BatchNorm2d(128)
-
-        self.d_conv3 = nn.ConvTranspose2d(128, 32, kernel_size=second_kernel, stride=second_stride,
-                                          output_padding=(0, 1))  # output_padding=1)
-        self.d_bn3 = nn.BatchNorm2d(32)
-
-        self.d_conv4 = nn.ConvTranspose2d(32, channels, kernel_size=first_kernel, stride=first_stride, output_padding=1)
-
-    def forward(self, z):
-        decoded = F.relu(self.d_bn1(self.d_conv1(z)))
-        decoded = F.relu(self.d_bn2(self.d_conv2(decoded)))
-        decoded = F.relu(self.d_bn3(self.d_conv3(decoded)))
-        decoded = self.d_conv4(decoded)
-        return torch.sigmoid(decoded)
 
 
 class ConvVAE4Fixed(Storeable, BaseVAE):
@@ -71,11 +15,68 @@ class ConvVAE4Fixed(Storeable, BaseVAE):
                  first_stride=2, second_kernel=5, second_stride=2):
         self.input_shape = input_shape
         self.z_size = z_size
-        encoder = Encoder(input_shape, z_size, input_channels, first_kernel, first_stride, second_kernel, second_stride)
-        decoder = Decoder(z_size, encoder.z_shape, output_channels, first_kernel, first_stride, second_kernel, second_stride)
+        encoder = self.Encoder(input_shape, z_size, input_channels, first_kernel, first_stride, second_kernel, second_stride)
+        decoder = self.Decoder(z_size, encoder.z_shape, output_channels, first_kernel, first_stride, second_kernel, second_stride)
         BaseVAE.__init__(self, encoder, decoder, variational)
         Storeable.__init__(self)
 
+    class Encoder(nn.Module):
+        def __init__(self, input_shape, z_size, channels=3, first_kernel=5, first_stride=2, second_kernel=5, second_stride=2):
+            nn.Module.__init__(self)
+            # batchnorm in autoencoding is a thing
+            # https://arxiv.org/pdf/1602.02282.pdf
+
+            from mentalitystorm.util import conv_output_shape
+
+            # encoder
+            self.e_conv1 = nn.Conv2d(channels, 32, kernel_size=first_kernel, stride=first_stride)
+            self.e_bn1 = nn.BatchNorm2d(32)
+            output_shape = conv_output_shape(input_shape, kernel_size=first_kernel, stride=first_stride)
+
+            self.e_conv2 = nn.Conv2d(32, 128, kernel_size=second_kernel, stride=second_stride)
+            self.e_bn2 = nn.BatchNorm2d(128)
+            output_shape = conv_output_shape(output_shape, kernel_size=second_kernel, stride=second_stride)
+
+            self.e_conv3 = nn.Conv2d(128, 128, kernel_size=second_kernel, stride=second_stride)
+            self.e_bn3 = nn.BatchNorm2d(128)
+            self.z_shape = conv_output_shape(output_shape, kernel_size=second_kernel, stride=second_stride)
+
+            self.e_mean = nn.Conv2d(128, z_size, kernel_size=self.z_shape, stride=1)
+            self.e_logvar = nn.Conv2d(128, z_size, kernel_size=self.z_shape, stride=1)
+
+        def forward(self, x):
+            encoded = F.relu(self.e_bn1(self.e_conv1(x)))
+            encoded = F.relu(self.e_bn2(self.e_conv2(encoded)))
+            encoded = F.relu(self.e_bn3(self.e_conv3(encoded)))
+            mean = self.e_mean(encoded)
+            logvar = self.e_logvar(encoded)
+            return mean, logvar
+
+
+    class Decoder(nn.Module):
+        def __init__(self, z_size, z_shape, channels=3, first_kernel=5, first_stride=2, second_kernel=5, second_stride=2):
+            nn.Module.__init__(self)
+
+            # decoder
+            self.d_conv1 = nn.ConvTranspose2d(z_size, 128, kernel_size=z_shape, stride=1)
+            self.d_bn1 = nn.BatchNorm2d(128)
+
+            self.d_conv2 = nn.ConvTranspose2d(128, 128, kernel_size=second_kernel, stride=second_stride,
+                                              output_padding=(1, 0))
+            self.d_bn2 = nn.BatchNorm2d(128)
+
+            self.d_conv3 = nn.ConvTranspose2d(128, 32, kernel_size=second_kernel, stride=second_stride,
+                                              output_padding=(0, 1))  # output_padding=1)
+            self.d_bn3 = nn.BatchNorm2d(32)
+
+            self.d_conv4 = nn.ConvTranspose2d(32, channels, kernel_size=first_kernel, stride=first_stride, output_padding=1)
+
+        def forward(self, z):
+            decoded = F.relu(self.d_bn1(self.d_conv1(z)))
+            decoded = F.relu(self.d_bn2(self.d_conv2(decoded)))
+            decoded = F.relu(self.d_bn3(self.d_conv3(decoded)))
+            decoded = self.d_conv4(decoded)
+            return torch.sigmoid(decoded)
 
 class ConvVAE4FixedSub(BaseVAE):
     """
@@ -112,8 +113,8 @@ class AtariVAE2DLatent(Storeable, BaseVAE):
                  first_stride=2, second_kernel=5, second_stride=2):
         self.input_shape = input_shape
         self.z_size = z_size
-        encoder = Encoder(input_shape, z_size, input_channels, first_kernel, first_stride, second_kernel, second_stride)
-        decoder = Decoder(z_size, encoder.z_shape, output_channels, first_kernel, first_stride, second_kernel, second_stride)
+        encoder = self.Encoder(input_shape, z_size, input_channels, first_kernel, first_stride, second_kernel, second_stride)
+        decoder = self.Decoder(z_size, encoder.z_shape, output_channels, first_kernel, first_stride, second_kernel, second_stride)
         BaseVAE.__init__(self, encoder, decoder, variational)
         Storeable.__init__(self)
 
@@ -125,50 +126,68 @@ class AtariVAE2DLatent(Storeable, BaseVAE):
 
             from mentalitystorm.util import conv_output_shape
 
+
+            s1 = 32
+            s2 = 64
             # encoder
-            self.e_conv1 = nn.Conv2d(channels, 32, kernel_size=first_kernel, stride=first_stride)
-            self.e_bn1 = nn.BatchNorm2d(32)
+            self.e_conv1 = nn.Conv2d(channels, s1, kernel_size=first_kernel, stride=first_stride)
+            self.e_bn1 = nn.BatchNorm2d(s1)
             output_shape = conv_output_shape(input_shape, kernel_size=first_kernel, stride=first_stride)
 
-            self.e_conv2 = nn.Conv2d(32, 128, kernel_size=second_kernel, stride=second_stride)
-            self.e_bn2 = nn.BatchNorm2d(128)
+            self.e_conv2 = nn.Conv2d(s1, s2, kernel_size=second_kernel, stride=second_stride)
+            self.e_bn2 = nn.BatchNorm2d(s2)
             output_shape = conv_output_shape(output_shape, kernel_size=second_kernel, stride=second_stride)
 
-            self.e_conv3 = nn.Conv2d(128, 128, kernel_size=second_kernel, stride=second_stride)
-            self.e_bn3 = nn.BatchNorm2d(128)
-            self.z_shape = conv_output_shape(output_shape, kernel_size=second_kernel, stride=second_stride)
+            self.e_conv3 = nn.Conv2d(s2, s2, kernel_size=second_kernel, stride=second_stride)
+            self.e_bn3 = nn.BatchNorm2d(s2)
+            output_shape = conv_output_shape(output_shape, kernel_size=second_kernel, stride=second_stride)
 
-            self.e_mean = nn.Conv2d(128, z_size, kernel_size=self.z_shape, stride=1)
-            self.e_logvar = nn.Conv2d(128, z_size, kernel_size=self.z_shape, stride=1)
+            self.e_conv4 = nn.Conv2d(s2, s2, kernel_size=2, stride=2)
+            self.e_bn4 = nn.BatchNorm2d(s2)
+            output_shape = conv_output_shape(output_shape, kernel_size=2, stride=2)
+
+            self.e_mean = nn.Conv2d(s2, z_size, kernel_size=2, stride=2)
+            self.e_logvar = nn.Conv2d(s2, z_size, kernel_size=2, stride=2)
+            self.z_shape = conv_output_shape(output_shape, kernel_size=2, stride=2)
 
         def forward(self, x):
             encoded = F.relu(self.e_bn1(self.e_conv1(x)))
             encoded = F.relu(self.e_bn2(self.e_conv2(encoded)))
             encoded = F.relu(self.e_bn3(self.e_conv3(encoded)))
-            mean = torch.sigmoid(self.e_mean(encoded)) * 6 - 3
-            logvar = torch.sigmiod(self.e_logvar(encoded)) * 2
+            encoded = F.relu(self.e_bn4(self.e_conv4(encoded)))
+            mean = (torch.sigmoid(self.e_mean(encoded)) * 6) - 3
+            logvar = torch.sigmoid(self.e_logvar(encoded)) * 3
             return mean, logvar
 
     class Decoder(nn.Module):
         def __init__(self, z_size, z_shape, channels=3, first_kernel=5, first_stride=2, second_kernel=5, second_stride=2):
             nn.Module.__init__(self)
 
+            s1 = 32
+            s2 = 64
+
+            self.d_convz = nn.ConvTranspose2d(z_size, s2, kernel_size=2, stride=2, output_padding=(1, 0))
+            self.d_bnz = nn.BatchNorm2d(s2)
+
+            self.d_conv1 = nn.ConvTranspose2d(s2, s2, kernel_size=2, stride=2, output_padding=1)
+            self.d_bn1 = nn.BatchNorm2d(s2)
             # decoder
-            self.d_conv1 = nn.ConvTranspose2d(z_size, 128, kernel_size=z_shape, stride=1)
-            self.d_bn1 = nn.BatchNorm2d(128)
+            #self.d_conv1 = nn.ConvTranspose2d(z_size, 128, kernel_size=z_shape, stride=1)
+            #self.d_bn1 = nn.BatchNorm2d(128)
 
-            self.d_conv2 = nn.ConvTranspose2d(128, 128, kernel_size=second_kernel, stride=second_stride,
+            self.d_conv2 = nn.ConvTranspose2d(s2, s2, kernel_size=second_kernel, stride=second_stride,
                                               output_padding=(1, 0))
-            self.d_bn2 = nn.BatchNorm2d(128)
+            self.d_bn2 = nn.BatchNorm2d(s2)
 
-            self.d_conv3 = nn.ConvTranspose2d(128, 32, kernel_size=second_kernel, stride=second_stride,
+            self.d_conv3 = nn.ConvTranspose2d(s2, s1, kernel_size=second_kernel, stride=second_stride,
                                               output_padding=(0, 1))  # output_padding=1)
-            self.d_bn3 = nn.BatchNorm2d(32)
+            self.d_bn3 = nn.BatchNorm2d(s1)
 
-            self.d_conv4 = nn.ConvTranspose2d(32, channels, kernel_size=first_kernel, stride=first_stride, output_padding=1)
+            self.d_conv4 = nn.ConvTranspose2d(s1, channels, kernel_size=first_kernel, stride=first_stride, output_padding=1)
 
         def forward(self, z):
-            decoded = F.relu(self.d_bn1(self.d_conv1(z)))
+            decoded = F.relu(self.d_bnz(self.d_convz(z)))
+            decoded = F.relu(self.d_bn1(self.d_conv1(decoded)))
             decoded = F.relu(self.d_bn2(self.d_conv2(decoded)))
             decoded = F.relu(self.d_bn3(self.d_conv3(decoded)))
             decoded = self.d_conv4(decoded)
--- run_big_latent.py
+++ run_big_latent.py
@@ -3,7 +3,7 @@ from mentalitystorm.data import AutoEncodeSelect, StandardSelect
 from mentalitystorm import config, MseKldLoss, ImageViewer, DataPackage, Run, SimpleRunFac, Params, Handles, BceKldLoss
 import torchvision
 import torchvision.transforms as TVT
-from models import ConvVAE4Fixed
+from models import ConvVAE4Fixed, AtariVAE2DLatent
 from tqdm import tqdm
 from torch.optim import Adam
 from mentalitystorm.atari import GymImageDataset
@@ -12,6 +12,7 @@ if __name__ == '__main__':
 
     input_viewer = ImageViewer('input', (320, 480))
     output_viewer = ImageViewer('output', (320, 480))
+    latent_viewer = ImageViewer('latent', (320, 480))
     latent_instr = LatentInstrument()
 
     invaders = torchvision.datasets.ImageFolder(
@@ -43,57 +44,41 @@ if __name__ == '__main__':
                                        input_transform=TVT.Compose([TVT.ToTensor()]),
                                        target_transform=TVT.Compose([TVT.ToTensor()]))
 
-    co_ord_conv_data_package_w_target = DataPackage(co_ord_conv_invaders_w_target, StandardSelect())
     co_ord_conv_data_package = DataPackage(co_ord_conv_invaders, StandardSelect())
     control_data_package = DataPackage(regular_invaders, AutoEncodeSelect())
 
     run_fac = SimpleRunFac()
-    model = Params(ConvVAE4Fixed, (210, 160), 32, input_channels=3, output_channels=3)
-    model3 = Params(ConvVAE4Fixed, (210, 160), 32, input_channels=5, output_channels=3)
-    model5 = Params(ConvVAE4Fixed, (210, 160), 32, input_channels=5, output_channels=5)
+    model = Params(AtariVAE2DLatent, (210, 160), 32, input_channels=5, output_channels=3)
+    #model3 = Params(ConvVAE4Fixed, (210, 160), 32, input_channels=5, output_channels=3)
     #model = Params(ConvVAE4Fixed, (400, 600), 2)
 
     opt = Params(Adam, lr=1e-3)
-    run_fac.run_list.append(Run(model, opt, Params(BceKldLoss), control_data_package, run_name='control BCE'))
-    run_fac.run_list.append(Run(model, opt, Params(MseKldLoss), control_data_package, run_name='control MSE'))
-    run_fac.run_list.append(Run(model3, opt, Params(BceKldLoss), co_ord_conv_data_package, run_name='co-ord conv BCE'))
-    run_fac.run_list.append(Run(model3, opt, Params(MseKldLoss), co_ord_conv_data_package, run_name='co-ord conv MSE'))
-    #run_fac.run_list.append(Run(model5, opt, Params(MseKldLoss), co_ord_conv_data_package_w_target, run_name='co-ord conv channel output'))
-    #run_fac.run_list.append(Run(model, opt, Params(MseKldLoss, beta=8.0), data_package, run_name='B-VAE loss 8.0'))
+    #run_fac.run_list.append(Run(model, opt, Params(BceKldLoss), control_data_package, run_name='control BCE'))
+    run_fac.run_list.append(Run(model, opt, Params(MseKldLoss), co_ord_conv_data_package, run_name='biglatent MSE with co-conv'))
 
-    #run_fac = SimpleRunFac.resume(r'C:\data\runs\431', data_package)
+    #run_fac = SimpleRunFac.resume(r'C:\data\runs\489', co_ord_conv_data_package)
 
-    batch_size = 60
-    epochs = 20
+    batch_size = 64
+    epochs = 100
 
     for model, opt, loss_fn, data_package, trainer, tester, run in run_fac:
         dev, train, test, selector = data_package.loaders(batch_size=batch_size)
 
         model.register_forward_hook(input_viewer.view_input)
+        model.decoder.register_forward_hook(latent_viewer.view_input)
         model.register_forward_hook(output_viewer.view_output)
         register_tb(run)
 
         for epoch in tqdm(run.for_epochs(epochs), 'epochs', epochs):
 
-            epoch.register_after_hook(latent_instr.write_correlation)
-            epoch.register_after_hook(write_histogram)
+            #epoch.register_after_hook(write_histogram)
             epoch.execute_before(epoch)
 
             trainer.train(model, opt, loss_fn, train, selector, run, epoch)
 
             handles = Handles()
-            handles += model.encoder.register_forward_hook(latent_instr.store_latent_vars_in_epoch)
             handles += loss_fn.register_hook(tb_test_loss_term)
             tester.test(model, loss_fn, test, selector, run, epoch)
             handles.remove()
             epoch.execute_after(epoch)
             run.save()
-
-
-
-
-
-
-
-
-
